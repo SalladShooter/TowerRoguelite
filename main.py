2a3aa553fe8458ff8354a7b33b5f6e298a21278b
SalladShooter <139503084+SalladShooter@users.noreply.github.com>
2026-01-15 14:38:06 -0600

    Setup basic player and moves

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# Hello World
+import pygame as py
+from player import Player
+from moves import Moves
+
+py.init()
+screen = py.display.set_mode((1280,720))
+clock = py.time.Clock()
+running = True
+
+all_sprites = py.sprite.Group()
+moves = py.sprite.Group()
+
+player = Player()
+all_sprites.add(player)
+
+for i in range(5):
+    move = Moves()
+    moves.add(move)
+    all_sprites.add(move)
+
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    screen.fill("black")
+
+    all_sprites.update()
+
+    py.display.flip()
+
+    clock.tick(60)
+
+py.quit()
---
+++ moves.py
@@ -0,0 +1,5 @@
+import pygame as py
+
+class Moves(py.sprite.Sprite):
+    def __init__(self):
+        py.sprite.Sprite.__init__(self)
